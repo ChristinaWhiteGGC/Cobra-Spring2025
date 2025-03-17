633fb480c73546258eef38077377dd6218d24e03
William <wchokbengboune@ggc.edu>
2025-03-17 00:10:24 -0400

    Added line that prints name

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is William");
+    }
     // Teresa's daughter is the mother of my daughter, who am I to Teresa?
     // This is the main class, work in progress!!
 }
