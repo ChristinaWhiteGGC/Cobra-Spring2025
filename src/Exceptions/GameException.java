161bac99fc3bbad641b518f0fec10281f4a589d8
ChristinaWhiteGGC <cmaher2@ggc.edu>
2025-04-20 23:50:36 -0400

    1) updated artifact.txt

--- src/Exceptions/GameException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-
-// A custom exception that can be caught, displays a message to the user, and resets state
-
-public class GameException extends Exception {
-    public GameException(String message) {
-        super(message);
-    }
-}
